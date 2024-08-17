3bb225747d6806137b577a3ccbc7620367f23c46
Jose Danilo <danilojose.1d@gmail.com>
2024-08-17 15:31:44 -0300

    Verificação de caracter valido

--- busca_struzik.c
+++ busca_struzik.c
@@ -1,48 +1,16 @@
-#include <string.h> //para utilização da função: strcmp
 #include <stdio.h>
-#include <ctype.h>
 
-int buscaExponencial(char **vetor, int tamanho, char *valor)
-{
-    int i = 1;
-    // Encontra o intervalo onde o valor pode estar
-    while (i < tamanho && strcmp(vetor[i], valor) <= 0)
-    {
-        i *= 2;
-    }
-    int inicio = i / 2;
-    int fim = i < tamanho ? i : tamanho; // Garante que o limite não ultrapasse o tamanho do vetor
-    // Busca linear no intervalo
-    for (int j = inicio; j < fim; j++)
-    {
-        if (strcmp(vetor[j], valor) == 0)
-        {
-            return j;
-        }
-    }
-    return -1; // Se o valor não for encontrado, retorna -1
-}
 
-int main(void)
-{
-    char *vetor[] = {"ALICE", "BRUNO", "CLAUDIO", "DANILO", "EMERSON"};
-    char valor[10];
-    printf("Informe um nome: ");
-    scanf(" %[^\n]", valor);
-    for(int i = 0; i < strlen(valor); i++){
-        valor[i] = toupper(valor[i]);
-    }
-    int tamanho = sizeof(vetor) / sizeof(vetor[0]);
-    int posicao = buscaExponencial(vetor, tamanho, valor);
+int main(void){
 
-    if (posicao != -1)
-    {
-        printf("O nome esta na posiçao %d\n", posicao);
-    }
-    else
-    {
-        printf("Posição não encontrada!\n");
-    }
+int i = 'z';
+printf("%d", i);
+
+
+
+
+
+
 
     return 0;
 }
